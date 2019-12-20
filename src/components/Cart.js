dfb1a3d577eaf954c78a753c9f50a4b9b7cc1a0a
Dmitrii-Lobanov <dmitriilobanov3@gmail.com>
2019-12-20 09:36:38 +0500

    Modals added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Modal from 'react-modal';
 import Product from './Product'
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    maxWidth              : '700px',
+    maxHeight             : '550px'
+  }
+};
+
+Modal.setAppElement('#root')
+
 const Cart  = ({ products, total, onCheckoutClicked }) => {
+  const [payButton, setPayButton] = useState('Оплатить');
+
   const hasProducts = products.length > 0
   const nodes = hasProducts ? (
     products.map(product =>
@@ -18,15 +36,54 @@ const Cart  = ({ products, total, onCheckoutClicked }) => {
     <em>Добавленные в корзину товары</em>
   )
 
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#000';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+  function onPayButton() {
+    return setPayButton('Оплачено');
+  }
+
   return (
     <div>
       <h3>Корзина</h3>
       <div>{nodes}</div>
       <p>Итого: {total} рублей</p>
-      <button onClick={onCheckoutClicked}
+      <button onClick={openModal}
         disabled={hasProducts ? '' : 'disabled'}>
         К оплате
       </button>
+      <Modal 
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Modal"
+      >
+        <button onClick={closeModal}>Назад в магазин</button>
+        <div ref={_subtitle => (subtitle = _subtitle)} className='desc-modal-content'>
+          <h2>Корзина</h2>
+          <div>{nodes}</div>
+          <p>Итого: {total} рублей</p>
+          <button onClick={onPayButton}>
+            {payButton}
+          </button>
+        </div>
+        <div>
+      </div>
+      <button onClick={closeModal}>Назад в магазин</button>
+    </Modal>
     </div>
   )
 }
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,8 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Modal from 'react-modal'
+
 import Product from './Product'
 
-const ProductItem = ({ product, onAddToCartClicked }) => (
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    maxWidth              : '700px',
+    maxHeight             : '550px'
+  }
+};
+
+Modal.setAppElement('#root')
+
+const ProductItem = ({ product, onAddToCartClicked }) => {
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#000';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+  return (
   <div>
   <img src={require(`../data/${product.id}.jpg`)} width='200' />
     <Product
@@ -10,13 +43,40 @@ const ProductItem = ({ product, onAddToCartClicked }) => (
       title={product.title}
       price={product.price}
       quantity={product.inventory} />
+      <button onClick={openModal}>
+        Подробнее
+      </button>
+      <Modal 
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Modal"
+      >
+        <button onClick={closeModal}>Назад в магазин</button>
+        <div ref={_subtitle => (subtitle = _subtitle)} className='desc-modal-content'>
+          <h2>{product.title}</h2>
+          <img src={require(`../data/${product.id}.jpg`)} width="400" />
+          <h3>{product.price} рублей</h3>
+          <p>{product.description}</p>
+        </div>
+        <div>
+          <button
+            onClick={onAddToCartClicked}
+            disabled={product.inventory > 0 ? '' : 'disabled'} >
+            {product.inventory > 0 ? 'Добавить в корзину' : 'Продано'}
+          </button>
+        </div>
+        <button onClick={closeModal}>Назад в магазин</button>
+      </Modal>
     <button
       onClick={onAddToCartClicked}
       disabled={product.inventory > 0 ? '' : 'disabled'} >
       {product.inventory > 0 ? 'Добавить' : 'Продано'}
     </button>
   </div>
-)
+  );
+}
 
 ProductItem.propTypes = {
   product: PropTypes.shape({
